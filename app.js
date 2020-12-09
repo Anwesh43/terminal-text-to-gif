d9d209785ff1d079d175a501bfd7115f45481f96
Anwesh43 <anweshthecool0@gmail.com>
2020-12-10 03:23:05 +0530

    adding TextGif class

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const gifencoder = require('gifencoder')
+const GifEncoder = require('gifencoder')
 const Canvas = require('canvas')
+const GIFEncoder = require('gifencoder')
+const {createWriteStream} = require('fs')
 const w = 500, h = 500
 const backColor = "#BDBDBD"
+const delay = 50 
 
 class Stage {
 
@@ -12,8 +15,42 @@ class Stage {
         this.context = this.canvas.getContext('2d')
     }
 
-    render() {
+    render(cb) {
         this.context.fillStyle = backColor
         this.context.fillRect(0, 0, w, h)
+        cb(this.context)
+    }
+
+    update(cb) {
+        
+    }
+}
+
+class TextGif {
+
+    constructor() {
+        this.encoder = new GIFEncoder(w, h)
+        this.encoder.setQuality(100)
+        this.encoder.setDelay(delay)
+        this.encoder.setRepeat(0)
+        this.started = false
+        this.stage = new Stage()
+    }
+
+    create(fileName) {
+        if (this.started) {
+          return 
+        }
+        this.started = true 
+        this.encoder.createReadStream().pipe(createWriteStream(fileName))
+        this.encoder.start()
+        while (this.started) {
+            this.stage.render((context) => {
+                this.encoder.addFrame(context)
+            })
+            this.stage.update(() => {
+                this.encoder.end()
+            })
+        }
     }
 }
